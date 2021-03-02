af18c4a4e23ea6e48b184473860cb6e4b2eeddfc
TBoltsp <sylvain.paulet01@gmail.com>
2021-03-02 12:20:34 +0100

    debut pages

---
+++ app/src/main/java/com/example/p222appli/Activity_WasteInfos.java
@@ -0,0 +1,14 @@
+package com.example.p222appli;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_WasteInfos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__waste_infos);
+    }
+}
---
+++ app/src/main/java/com/example/p222appli/Activity_WasteSorting.java
@@ -0,0 +1,14 @@
+package com.example.p222appli;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_WasteSorting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__waste_sorting);
+    }
+}
