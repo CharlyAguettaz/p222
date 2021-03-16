cbbf0765414a124282b764ca84e27f79c0f8fe27
TBoltsp <sylvain.paulet01@gmail.com>
2021-03-16 11:37:07 +0100

    adaptations string multilangues

--- app/src/main/java/com/example/p222appli/Activity_Inscriptions.java
+++ app/src/main/java/com/example/p222appli/Activity_Inscriptions.java
@@ -2,11 +2,13 @@ package com.example.p222appli;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,25 +17,27 @@ public class Activity_Inscriptions extends AppCompatActivity implements View.OnC
 
     //private TextView pointsProfil;
     private DatabaseManager databaseManager;
-    public Button bt_validerIns;
-
+    Button bt_connexion;
+    Button bt_valider;
     private EditText name, mail, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity__login);
+        setContentView(R.layout.activity__inscriptions);
 
-        bt_validerIns = (Button) findViewById(R.id.bt_valider);
+        bt_valider = (Button) findViewById(R.id.bt_valider);
         name = (EditText) findViewById(R.id.et_name);
         mail = (EditText) findViewById(R.id.et_mail);
         password = (EditText) findViewById(R.id.et_password);
-        bt_validerIns.setOnClickListener(this);
+        bt_valider.setOnClickListener(this);
     }
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.bt_valider) {
+            Toast.makeText(getApplicationContext(), "Know connect yourself", Toast.LENGTH_LONG).show();
             Intent i = new Intent(this, Activity_Login.class);
 
             databaseManager = new DatabaseManager(this);
